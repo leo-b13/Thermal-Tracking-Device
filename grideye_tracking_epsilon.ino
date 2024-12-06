2b7da4dc7073c7a5c292bfe22dc85dbd16177fc2
leo-b13 <leo.burton2113@gmail.com>
2024-12-06 11:12:07 -0500

    Update grideye_tracking_epsilon.ino

--- grideye_tracking_epsilon.ino
+++ grideye_tracking_epsilon.ino
@@ -63,7 +63,7 @@ void setup() {
 
 
   //PID Variables
-  Kp = 14;
+  Kp = 12;
   //Ki = 0.5;
   //Kd = 0.005;
   last_time = 0;
@@ -143,7 +143,9 @@ void TrackingFunc() {
   output = pid(error);                                 //Goes to PID function to calculate error output
   pos += output;                                       
   pos = constrain(pos, minServoAngle, maxServoAngle);  //Constrains position variable between 0 and 180 to prevent stress on motor
-  xPos = (180 - pos) + 30;
+  int pos_adj = 16;
+  float pos_adj_2 = (pos-90) / 6;
+  xPos = (180 - pos) + pos_adj + pos_adj_2;
   track_servo.write(pos); 
   x_servo.write(xPos);
   //debugFunction(centerX, error, output, pos, dt, current_time);  //Debug function, comment out/in
@@ -240,4 +242,3 @@ void debugFunction(float centerX, double error, double output, int pos, long dt,
   Serial.println(dt);
   Serial.println("------------------------\n");
 }
-
